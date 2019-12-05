4824ce547a347dc63bc1f61f69a52a6028e60a43
Brian Denicola <bjd145@yahoo.com>
2019-12-05 07:55:55 -0600

    New script to test build

---
+++ Scripts/create_keys.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+export numKeys=$1
+
+ip=`kubectl get service traefik -o jsonpath={.status.loadBalancer.ingress[].ip}`
+curl --header "Content-Type: application/json" --request POST --data "{\"NumberOfKeys\":${numKeys}}"  http://${ip}/api/keys
