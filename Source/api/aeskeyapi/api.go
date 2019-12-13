6c3d8a1cf3793d2c853ac96ee55cfa81b88e34df
Brian Denicola <bjd145@yahoo.com>
2019-12-13 09:24:16 -0600

    Oops

--- Source/api/aeskeyapi/api.go
+++ Source/api/aeskeyapi/api.go
@@ -52,6 +52,7 @@ func (a *AESApi) InitHTTPServer(port string) {
 func (a *AESApi) logRequest (handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
 	})
 }
 
