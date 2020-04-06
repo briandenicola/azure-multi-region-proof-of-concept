5379e2a9b99bb9c3f7644541112367e5fb4c1d3c
Brian Denicola <bjd145@yahoo.com>
2020-04-06 10:22:40 -0500

    Merge Deployments

--- Infrastructure/create_infrastructure.sh
+++ Infrastructure/create_infrastructure.sh
@@ -6,20 +6,20 @@ while (( "$#" )); do
       RG=$2
       shift 2
       ;;
-    -l|--location)
-      location=$2
-      shift 2
-      ;;
-    -s|--subscription)
-      subscription=$2
+    -r|--region)
+      regions+=($2)
       shift 2
       ;;
     -n|--name)
       appName=$2
       shift 2
       ;;
+    --client-secret)
+      CLIENT_SECRET=$2
+      shift 2
+      ;;
     -h|--help)
-      echo "Usage: ./create_infrastructure.sh -n {App Name} -g {Resource Group} -l {location} -s {Subscription Name}"
+      echo "Usage: ./create_infrastructure.sh -n {App Name} -g {Resource Group} -r {region} --client-secret {SPN secret} [-r {secondary region}]"
       exit 0
       ;;
     --) 
@@ -49,79 +49,142 @@ if [[ -z "${appName}" ]]; then
   appName=`cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1`
 fi 
 
-cosmosDBAccountName=db${appName}001
-eventHubNameSpace=hub${appName}001
-redisName=cache${appName}001
-aks=k8s${appName}001
-nodeRG=${RG}_nodes 
-storageAccountName=${appName}sa001
-acrAccountName=acr${appName}001
-appInsightsName=ai${appName}001
-logAnalyticsWorkspace=logs${appName}001
+if [[ -z "${regions[0]}" ]]; then
+  echo "This script requires at least one region defined"
+  exit 1
+fi 
+primary=${regions[0]}
+
+if [[ -z "${RG}" ]]; then
+  echo "This script requires a Resource Group name defined"
+  exit 1
+fi 
 
 az account show  >> /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
   az login
 fi
 
-#Get Subscription Id
-az account set -s ${subscription}
-
 #Add extensions
 az extension add --name application-insights
 az extension add --name log-analytics
 
 #Create Resource Group
-az group create -n $RG -l $location
+az group create -n $RG -l $primary
+
+#Resource Names
+cosmosDBAccountName=db${appName}001
+acrAccountName=acr${appName}001
+appInsightsName=ai${appName}001
+logAnalyticsWorkspace=logs${appName}001
+
+#Variables
+CLIENT_ID='4e565daf-621d-48d3-b010-1208da519cbe'
 
 #Create Cosmos
 database=AesKeys
 collection=Items
 
-az cosmosdb create -g ${RG} -n ${cosmosDBAccountName} --kind GlobalDocumentDB 
+cosmosdb_cmd="az cosmosdb create -g ${RG} -n ${cosmosDBAccountName} --kind GlobalDocumentDB --enable-multiple-write-locations"
+failoverPriority=0
+for region in ${regions[@]}
+do
+  cosmosdb_cmd+=" --locations regionName=${region} failoverPriority=${failoverPriority}"
+  failoverPriority=$((failoverPriority+1))
+done
+$cosmosdb_cmd
+
 az cosmosdb sql database create  -g ${RG} -a ${cosmosDBAccountName} -n ${database}
 az cosmosdb sql container create -g ${RG} -a ${cosmosDBAccountName} -d ${database} -n ${collection} --partition-key-path '/keyId'
 
-#Create Event Hub
-hub=events
-az eventhubs namespace create -g ${RG} -n ${eventHubNameSpace} -l ${location} --sku Standard --enable-auto-inflate --maximum-throughput-units 5 --enable-kafka
-az eventhubs eventhub create -g ${RG} --namespace-name ${eventHubNameSpace} -n ${hub} --message-retention 7 --partition-count 1
-
-#Create Redis Cache
-az redis create  -g ${RG} -n ${redisName} -l ${location} --sku standard --vm-size c1 --minimum-tls-version 1.2   
-
-#Create Azure Storage
-az storage account create --name ${storageAccountName} --location $location --resource-group $RG --sku Standard_LRS
-
 #Create ACR
-az acr create -n ${acrAccountName} -g ${RG} -l ${location} --sku Standard
+az acr create -n ${acrAccountName} -g ${RG} -l ${primary} --sku Premium
 acrid=`az acr show -n ${acrAccountName} -g ${RG} --query 'id' -o tsv`
 
-#Create AKS
-az aks create -n ${aks} -g ${RG} -l ${location} --load-balancer-sku standard --node-count 3 --node-resource-group ${nodeRG} --ssh-key-value '~/.ssh/id_rsa.pub' 
-az aks update -n ${aks} -g ${RG} --enable-acr --acr ${acrid}    
-
 # Create Application Insights
-az monitor app-insights component create --app ${appInsightsName} --location ${location} --kind web -g ${RG} --application-type web
+az monitor app-insights component create --app ${appInsightsName} --location ${primary} --kind web -g ${RG} --application-type web
 
 # Create Log Analytics Workspace 
-az monitor log-analytics workspace create -n ${logAnalyticsWorkspace} --location ${location} -g ${RG}
+az monitor log-analytics workspace create -n ${logAnalyticsWorkspace} --location ${primary} -g ${RG}
 
-## Get Pod Credentials 
-az aks get-credentials -n ${aks} -g ${RG} 
+count=1
+for region in ${regions[@]}
+do
 
-if [[ $? -eq 0 ]]; then
-  ## Install Traefik Ingress 
-  helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-  helm repo update
-  helm install traefik stable/traefik --set rbac.enabled=true
+  #Resource Names
+  vnetName=vnet${appName}00${count}
+  eventHubNameSpace=hub${appName}00${count}
+  redisName=cache${appName}00${count}
+  aks=k8s${appName}00${count}
+  storageAccountName=${appName}sa00${count}
+  nodeRG=${RG}_${region}_nodes 
 
-  ## Install Keda
-  helm repo add kedacore https://kedacore.github.io/charts
-  helm repo update
-  kubectl create namespace keda
-  helm install keda kedacore/keda --namespace keda
-fi 
+  #ACR Update
+  if [ $region != $primary ]; then 
+    az acr replication create -r ${acrAccountName} -l ${region}
+  fi
+
+  #Create Event Hub
+  hub=events
+  az eventhubs namespace create -g ${RG} -n ${eventHubNameSpace} -l ${region} --sku Standard --enable-auto-inflate --maximum-throughput-units 5 --enable-kafka
+  az eventhubs eventhub create -g ${RG} --namespace-name ${eventHubNameSpace} -n ${hub} --message-retention 7 --partition-count 1
+
+  #Create Redis Cache
+  az redis create  -g ${RG} -n ${redisName} -l ${region} --sku standard --vm-size c1 --minimum-tls-version 1.2   
+
+  #Create Azure Storage
+  az storage account create --name ${storageAccountName} --resource-group $RG --sku Standard_LRS -l ${region}
+
+  #Create Virtual Network and Subnets
+  vnetIPRange="10.${count}.0.0/16"
+  appgwSubnet=AppGateway
+  appgwSubnetRange="10.${count}.1.0/24"
+  az network vnet create -g ${RG} -n ${vnetName} -l ${region} --address-prefix ${vnetIPRange} --subnet-name ${appgwSubnet} --subnet-prefix ${appgwSubnetRange} 
+
+  apimSubnet=APIM
+  apimSubnetRanage="10.${count}.2.0/24"
+  az network vnet subnet create -g ${RG} --vnet-name ${vnetName} -n ${apimSubnet} --address-prefixes ${apimSubnetRanage}
+
+  k8sSubnet=Kubernetes
+  k8sSubnetRange="10.${count}.4.0/22"
+  k8ssubnetid=`az network vnet subnet create -g ${RG} --vnet-name ${vnetName} -n ${k8sSubnet} --address-prefixes ${k8sSubnetRange} --query 'id' -o tsv`
+
+  #Create AKS
+  SERVICE_CIDR="10.19${count}.0.0/16"
+  DNS_IP="10.19${count}.0.10"
+  
+  az aks create -n ${aks} -g ${RG} -l ${region} \
+    --load-balancer-sku standard \
+    --node-count 3 \
+    --node-resource-group ${nodeRG} \
+    --ssh-key-value '~/.ssh/id_rsa.pub' \
+    --vnet-subnet-id ${k8ssubnetid} \
+    --service-cidr $SERVICE_CIDR \
+    --dns-service-ip $DNS_IP \
+    --network-plugin azure \
+    --service-principal $CLIENT_ID \
+    --client-secret $CLIENT_SECRET \
+    --location ${region}
+  az aks update -n ${aks} -g ${RG} --enable-acr --acr ${acrid}    
+
+  ## Get Pod Credentials 
+  az aks get-credentials -n ${aks} -g ${RG} 
+
+  if [[ $? -eq 0 ]]; then
+    ## Install Traefik Ingress 
+    helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+    helm repo update
+    helm upgrade -i traefik stable/traefik --set rbac.enabled=true --set service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal"=true
+ 
+    ## Install Keda
+    helm repo add kedacore https://kedacore.github.io/charts
+    helm repo update
+    kubectl create namespace keda
+    helm upgrade -i keda kedacore/keda --namespace keda
+  fi
+
+  count=$((count+1))
+done 
 
 # echo Application name
 echo ------------------------------------
--- Infrastructure/deploy_application.sh
+++ Infrastructure/deploy_application.sh
@@ -2,10 +2,6 @@
 
 while (( "$#" )); do
   case "$1" in
-    -s|--subscription)
-      subscription=$2
-      shift 2
-      ;;
     -n|--name)
       appName=$2
       shift 2
@@ -14,13 +10,17 @@ while (( "$#" )); do
       RG=$2
       shift 2
       ;;
-    -h|--help)
-      echo "Usage: ./deploy_application.sh -n {App Name} -g {Resource Group} -p {Primary Location} -s {Secondary Location} -v {Version. Default=1.0}"
-      exit 0
+    -r|--region)
+      regions+=($2)
+      shift 2
       ;;
     -v|--version)
       version=$2
       shift 2
+      ;;
+    -h|--help)
+      echo "Usage: ./deploy_application.sh -n {App Name} -g {Resource Group} -r {region} -v {Version. Default=1.0}  [-r {secondary region}]"
+      exit 0
       ;;
     --) 
       shift
@@ -35,10 +35,8 @@ done
 
 acrAccountName=acr${appName}001
 cosmosDBAccountName=db${appName}001
-eventHubNameSpace=hub${appName}001
-redisName=cache${appName}001
-storageAccountName=${appName}sa001
 appInsightsName=ai${appName}001
+eventHub=events
 
 az account show  >> /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
@@ -49,32 +47,14 @@ if [[ -z "${version}" ]]; then
   version="1.0"
 fi 
 
+if [[ -z "${regions[0]}" ]]; then
+  echo "This script requires at least one region defined"
+  exit 1
+fi 
+primary=${regions[0]}
+
 #Set Subscription and login into ACR
-az account set -s ${subscription}
 az acr login -n ${acrAccountName}
-
-## Get Cosmos Connection String
-cosmosConnectionString=`az cosmosdb list-connection-strings -n ${cosmosDBAccountName} -g ${RG} --query 'connectionStrings[0].connectionString' -o tsv`
-cosmosEncoded=`echo -n ${cosmosConnectionString} | base64 -w 0`
-
-## Get Event Hub Connection String 
-eventHub=events
-ehConnectionString=`az eventhubs namespace authorization-rule keys list -g ${RG} --namespace-name ${eventHubNameSpace} --name RootManageSharedAccessKey -o tsv --query primaryConnectionString`
-eventHubEncoded=`echo -n "${ehConnectionString};EntityPath=${eventHub}" | base64 -w 0`
-
-## Get Redis Connection String
-redisKey=`az redis list-keys  -g ${RG} -n ${redisName} -o tsv --query primaryKey`
-redisConnectionString=${redisName}.redis.cache.windows.net:6380,password=${redisKey},ssl=True,abortConnect=False
-redisEncoded=`echo -n ${redisConnectionString} | base64 -w 0`
-
-## Get Azure Storage Connection String
-storageKey=`az storage account keys list -n ${storageAccountName} --query '[0].value' -o tsv`
-storageConnectionString="DefaultEndpointsProtocol=https;AccountName=${storageAccountName};AccountKey=${storageKey}"
-storageEncoded=`echo -n "${storageConnectionString};EndpointSuffix=core.windows.net" | base64 -w 0`
-
-## Get Application Insight Key
-instrumentationKey=`az monitor app-insights component  show --app ${appInsightsName} -g ${RG} --query instrumentationKey -o tsv`
-instrumentationKeyEncoded=`echo -n ${instrumentationKey} | base64 -w 0`
 
 cd ..
 cwd=`pwd`
@@ -89,8 +69,44 @@ docker build -t ${acrAccountName}.azurecr.io/cqrs/eventprocessor:${version} .
 docker push ${acrAccountName}.azurecr.io/cqrs/eventprocessor:${version} 
 cd ${cwd}
 
+## Get Cosmos Connection String
+cosmosConnectionString=`az cosmosdb list-connection-strings -n ${cosmosDBAccountName} -g ${RG} --query 'connectionStrings[0].connectionString' -o tsv`
+cosmosEncoded=`echo -n ${cosmosConnectionString} | base64 -w 0`
+
+## Get Application Insight Key
+instrumentationKey=`az monitor app-insights component  show --app ${appInsightsName} -g ${RG} --query instrumentationKey -o tsv`
+instrumentationKeyEncoded=`echo -n ${instrumentationKey} | base64 -w 0`
+
 cd Deployment
-helm upgrade --install \
+
+count=1
+for region in ${regions[@]}
+do
+
+  eventHubNameSpace=hub${appName}00${count}
+  redisName=cache${appName}00${count}
+  aks=k8s${appName}00${count}
+  storageAccountName=${appName}sa00${count}
+
+  ## Get Redis Connection String
+  redisKey=`az redis list-keys  -g ${RG} -n ${redisName} -o tsv --query primaryKey`
+  redisConnectionString=${redisName}.redis.cache.windows.net:6380,password=${redisKey},ssl=True,abortConnect=False
+  redisEncoded=`echo -n ${redisConnectionString} | base64 -w 0`
+
+  ## Get Azure Storage Connection String
+  storageKey=`az storage account keys list -n ${storageAccountName} --query '[0].value' -o tsv`
+  storageConnectionString="DefaultEndpointsProtocol=https;AccountName=${storageAccountName};AccountKey=${storageKey}"
+  storageEncoded=`echo -n "${storageConnectionString};EndpointSuffix=core.windows.net" | base64 -w 0`
+
+  ## Get Event Hub Connection String 
+  ehConnectionString=`az eventhubs namespace authorization-rule keys list -g ${RG} --namespace-name ${eventHubNameSpace} --name RootManageSharedAccessKey -o tsv --query primaryConnectionString`
+  eventHubEncoded=`echo -n "${ehConnectionString};EntityPath=${eventHub}" | base64 -w 0`
+
+  ## Switch K8S Context 
+  kubectl config use-context ${aks}
+
+  #Install App
+  helm upgrade --install \
     --set acr_name=${acrAccountName} \
     --set AzureWebJobsStorage=${storageEncoded} \
     --set EVENTHUB_CONNECTIONSTRING=${eventHubEncoded} \
@@ -100,3 +116,12 @@ helm upgrade --install \
     --set api_version=${version} \
     --set eventprocessor_version=${version} \
     cqrs .
+
+    count=$((count+1))
+done
+
+
+
+
+
+
