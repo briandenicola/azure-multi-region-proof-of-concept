728cdecba25875f5754aac07e344f100ec507eea
Brian Denicola <bjd145@yahoo.com>
2020-09-02 13:15:45 -0500

    Updated Backend policies

--- Infrastructure/product/Deploy.ps1
+++ Infrastructure/product/Deploy.ps1
@@ -15,10 +15,13 @@ param (
 )  
 
 $GlobalKeyPolicy = Get-Content -Raw -Path ".\policies\GlobalKeyPolicy.xml"
-$CreateKeyPolicy = Get-Content -Raw -Path ".\policies\CreateKeyPolicy.xml"
 $RateLimitPolicy = Get-Content -Raw -Path ".\policies\RateLimitPolicy.xml"
 $MockPolicy = Get-Content -Raw -Path ".\policies\MockPolicy.xml"
 
+$CreateKeyPolicy = Get-Content -Raw -Path ".\policies\CreateKeyPolicy.xml"
+$CreateKeyPolicy = $CreateKeyPolicy.
+    Replace('{{primaryBackendUrl}}', $primaryBackendUrl)
+    
 $opts = @{
     Name                = ("Deployment-{0}-{1}" -f $ResourceGroupName, $(Get-Date).ToString("yyyyMMddhhmmss"))
     ResourceGroupName   = $ResourceGroupName
