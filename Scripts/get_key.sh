aab94094a34e19401d25c0b648dc94b7c9066656
Brian Denicola <bjd145@yahoo.com>
2020-09-09 11:28:15 -0500

    Fixed get_keys to include HTTPS

--- Scripts/get_key.sh
+++ Scripts/get_key.sh
@@ -3,6 +3,10 @@
 export keyid=$1
 
 ip=`kubectl get service traefik -o jsonpath={.status.loadBalancer.ingress[].ip}`
+hostname=`kubectl get ingresses cqrsapitls -o jsonpath={.spec.rules[0].host}`
 
 echo Making request to http://${ip}/api/keys/${keyid}
-kubectl run --rm -it --restart=Never --image bjd145/utils:latest --env="ip=${ip}" --env="keyid=${keyid}" getkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET http://${ip}/api/keys/${keyid}'
+kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="ip=${ip}" --env="keyid=${keyid}" getkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET http://${ip}/api/keys/${keyid}'
+
+echo Making request to https://${hostname}/api/keys
+kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="hostname=${hostname}" --env="keyid=${keyid}" getkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET https://${hostname}/api/keys/${keyid}'
