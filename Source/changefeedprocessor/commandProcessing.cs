f0a84991e9b802231e402a13986ccc2765f91afe
Brian Denicola <brian@briandenicola.com>
2021-09-30 08:20:49 -0500

    Bug fixes

--- Source/changefeedprocessor/commandProcessing.cs
+++ Source/changefeedprocessor/commandProcessing.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json; 
-using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Fbeltrao.AzureFunctionExtensions;
@@ -12,32 +11,32 @@ using StackExchange.Redis;
 
 namespace Eventing
 {
-    public static class CosmosChangeFeedProcessor
+    public static class ChangeFeedProcessor
     {
         [FunctionName("CosmosChangeFeedProcessor")]
-        public static void CosmosChangeFeedProcessor(
+        public static async Task CosmosChangeFeedProcessor(
             [CosmosDBTrigger(
                 databaseName: "AesKeys", 
                 collectionName: "Items", 
-                ConnectionStringSetting: "COSMOSDB_CONNECTIONSTRING",
-                LeaseCollectionName: "leases",
-                LeaseCollectionPrefix:"blob",
-                CreateLeaseCollectionIfNotExists: true
+                ConnectionStringSetting = "COSMOSDB_CONNECTIONSTRING",
+                LeaseCollectionName =  "leases",
+                LeaseCollectionPrefix = "blob",
+                CreateLeaseCollectionIfNotExists = true
             )]IReadOnlyList<AesKey> changeStream,  
             
             [RedisOutput(
-                Connection: "%REDISCACHE_CONNECTIONSTRING%"
+                Connection = "%REDISCACHE_CONNECTIONSTRING%"
             )] IAsyncCollector<RedisOutput> cacheKeys,                
             
             ILogger log)
         {
-            if (inputStream == null || inputStream.Count <= 0) {
+            if (changeStream == null || changeStream.Count <= 0) {
                 log.LogInformation($"CosmosChangeFeedProcessor - Fired but inputStream is null");
                 return;
             }
 
             try {
-                log.LogInformation($"{inputStream.Count} - Documents will be added to Cache");
+                log.LogInformation($"{changeStream.Count} - Documents will be added to Cache");
 
                 foreach( var document in changeStream ) 
                 {
