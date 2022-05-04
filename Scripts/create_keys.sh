911683a4d569480e70daae9eda0b248799f5b01d
briandenicola <brian@briandenicola.com>
2022-05-04 12:01:47 -0500

    Bump scripts to utils pod version 3.7

--- Scripts/create_keys.sh
+++ Scripts/create_keys.sh
@@ -5,8 +5,8 @@ export numKeys=$1
 ip=`kubectl get service traefik -o jsonpath={.status.loadBalancer.ingress[].ip}`
 hostname=`kubectl get ingresses cqrsapitls -o jsonpath={.spec.rules[0].host}`
 
-echo Making request to http://${ip}/api/keys
-kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="ip=${ip}" --env="numKeys=${numKeys}" createkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request POST --data "{\"NumberOfKeys\":${numKeys}}"  http://${ip}/api/keys' | jq
+#echo Making request to http://${ip}/api/keys
+#kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="ip=${ip}" --env="numKeys=${numKeys}" createkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request POST --data "{\"NumberOfKeys\":${numKeys}}"  http://${ip}/api/keys' | jq
 
 echo Making request to https://${hostname}/api/keys
-kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="hostname=${hostname}" --env="numKeys=${numKeys}" createkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request POST --data "{\"NumberOfKeys\":${numKeys}}"  https://${hostname}/api/keys' | jq
+kubectl run --rm -it --restart=Never --image bjd145/utils:3.7 --env="hostname=${hostname}" --env="numKeys=${numKeys}" createkeys --command -- /bin/bash -c 'curl --header "Content-Type: application/json" --request POST --data "{\"NumberOfKeys\":${numKeys}}"  https://${hostname}/api/keys' | jq
--- Scripts/get_key.sh
+++ Scripts/get_key.sh
@@ -5,8 +5,8 @@ export keyid=$1
 ip=`kubectl get service traefik -o jsonpath={.status.loadBalancer.ingress[].ip}`
 hostname=`kubectl get ingresses cqrsapitls -o jsonpath={.spec.rules[0].host}`
 
-echo Making request to http://${ip}/api/keys/${keyid}
-kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="ip=${ip}" --env="keyid=${keyid}" getkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET http://${ip}/api/keys/${keyid}' | jq
+#echo Making request to http://${ip}/api/keys/${keyid}
+#kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="ip=${ip}" --env="keyid=${keyid}" getkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET http://${ip}/api/keys/${keyid}' | jq
 
 echo Making request to https://${hostname}/api/keys/${keyid}
-kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="hostname=${hostname}" --env="keyid=${keyid}" getkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET https://${hostname}/api/keys/${keyid}' | jq
+kubectl run --rm -it --restart=Never --image bjd145/utils:3.7 --env="hostname=${hostname}" --env="keyid=${keyid}" getkeys --command -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET https://${hostname}/api/keys/${keyid}'
