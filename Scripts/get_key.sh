f29e07bdddd9c902f997496debdc3f7189cef731
briandenicola <brian@briandenicola.com>
2023-09-12 15:10:12 -0500

    Version bumps

--- Scripts/get_key.sh
+++ Scripts/get_key.sh
@@ -9,4 +9,4 @@ hostname=`kubectl get ingresses cqrsapitls -o jsonpath={.spec.rules[0].host}`
 #kubectl run --rm -it --restart=Never --image bjd145/utils:2.2 --env="ip=${ip}" --env="keyid=${keyid}" getkeys -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET http://${ip}/api/keys/${keyid}' | jq
 
 echo Making request to https://${hostname}/api/keys/${keyid}
-kubectl run --rm -it --restart=Never --image bjd145/utils:3.7 --env="hostname=${hostname}" --env="keyid=${keyid}" getkeys --command -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET https://${hostname}/api/keys/${keyid}'
+kubectl run --rm -it --restart=Never --image bjd145/utils:3.16 --env="hostname=${hostname}" --env="keyid=${keyid}" getkeys --command -- /bin/bash -c 'curl --header "Content-Type: application/json" --request GET https://${hostname}/api/keys/${keyid}'
